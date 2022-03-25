4c077d296319ad4c34560afd84b335f486834cc5
William Lewis <93591514+wlewis42@users.noreply.github.com>
2022-03-25 15:11:22 -0500

    Update newClient.py

--- newClient.py
+++ newClient.py
@@ -25,69 +25,57 @@ else:
    print("Not a valid port number.")
    exit()
 
-# user_input = input("Enter a command: ")
+user_input = input("Enter a command: ")
 
 # create socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # connect
-try: 
+try:
    client_socket.connect(ip,port)
 except:
-   print(f"Could not connect using ip: {ip}, port: {port}")
-# create struct
-s = struct.Struct("> I I I")
+   print(f"Unable to connect to (IP, PORT): {ip}, {port}")
+   log = open(logfile,"a")
+   log.write(f"Unable to connect to (IP, PORT): {ip}, {port}")
+
+try:
+   client_socket.sendmsg("HELLO")
+except:
+   print(f"Unable to send 'HELLO'")
 
 # send commands
-def sendHello():
+def sendMsg(msg):
+   log = open(logfile,"a")
+   s = struct.Struct(">III"+msg.encode())
    version= 17
-   msg_type = 0
-   msg_len = len("HELLO")
-   header = (version, msg_type, msg_len)
-   packed = s.pack(*header)
+   if msg=='HELLO':
+      msg_type = 0
+   elif msg=='LIGHTON':
+      msg_type = 1
+   elif msg=='LIGHTOFF':
+      msg_type = 2
+   elif msg=='DISCONNECT':
+      msg_type = 3
+   msg_len = len(msg)
+   header = (version, msg_type, msg_len, msg.encode())
+   packed = s.pack(header)
    client_socket.sendall(packed)
-   client_socket.sendall("HELLO")
+   log.write(f"Sending Command {msg}")
 
-def sendLightOn():
-   version= 17
-   msg_type = 1
-   msg_len = len("LIGHTON")
-   header = (version, msg_type, msg_len)
-   packed = struct.pack(*header)
-   client_socket.sendall(packed)
-   client_socket.sendall("LIGHTON")
+# # receive messages
+# def receive(data):
+conn, addr = client_socket.accept()
+print(f"Received Connection (IP, PORT): {addr}, {conn}")
+header = conn.recv(struct.calcsize('>III'))
+version, msg_type, msg_len = struct.unpack('>III',header)
+msg = client_socket.recv(msg_len).decode() # receive data
 
-def sendLightOff():
-   version= 17
-   msg_type = 2
-   msg_len = len("LIGHTOFF")
-   header = (version, msg_type, msg_len)
-   packed = struct.pack(*header)
-   client_socket.sendall(packed)
-   client_socket.sendall("LIGHTOFF")
-
-def sendGoodbye():
-   version= 17
-   msg_type = 3
-   msg_len = len("DISCONNECT")
-   header = (version, msg_type, msg_len)
-   packed = struct.pack(*header)
-   client_socket.sendall(packed)
-   client_socket.sendall("DISCONNECT")
-
-# receive messages
-def receive():
-   header = client_socket.recv(struct.calcsize('>III'))
-   version, msg_type, msg_len = struct.unpack('>III', header)
-   data = client_socket.recv(msg_len) # receive data
-while True:
-   user_input = input("Enter a comand: ")
-   # send correct command
-   if (user_input=="HELLO"):
-      sendHello()
-   elif (user_input=="LIGHTON"):
-      sendLightOn()
-   elif (user_input=="LIGHTOFF"):
-      sendLightOff()
-   elif (user_input=="DISCONNECT"):
-      sendGoodbye()
+# send correct command
+if (user_input=="HELLO"):
+   sendMsg(user_input)
+elif (user_input=="LIGHTON"):
+   sendMsg(user_input)
+elif (user_input=="LIGHTOFF"):
+   sendMsg(user_input)
+elif (user_input=="DISCONNECT"):
+   sendMsg(user_input)
